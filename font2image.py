8c961e6221c6fa88a939fcedc4f4f92681b3e96a
Nick Watts <nick@nawatts.com>
2015-11-08 13:20:31 -0500

    Correct argparse usage. Validate color strings.

--- font2image.py
+++ font2image.py
@@ -7,7 +7,14 @@ import sys
 from PIL import Image, ImageDraw, ImageFont
 
 def color_hex_string(hex_string):
-    return (int(hex_string[0:2], 16), int(hex_string[2:4], 16), int(hex_string[4:6], 16))
+    if len(hex_string) != 6 and len(hex_string) != 8:
+        raise argparse.ArgumentTypeError("Color must be an RGB or RGBA hex string, 6 or 8 characters long")
+
+    for char in hex_string:
+        if char not in "ABCDEFabcdef0123456789":
+            raise argparse.ArgumentTypeError("Color must be an RGB or RGBA hex string.")
+
+    return tuple([int(hex_string[i:i+2], 16) for i in range(0, len(hex_string), 2)])
 
 def expand_path(path):
     return os.path.realpath(os.path.expandvars(os.path.expanduser(path)))
@@ -20,29 +27,28 @@ parser.add_argument("font_file", type=expand_path,
 parser.add_argument("glyph", type=str,
     help="The Unicode code of the character to render. Ex: f0f3.")
 
-parser.add_argument("--background-color", nargs="?", default="ffffff",
+parser.add_argument("--background-color", default="ffffff",
     dest="background_color", type=color_hex_string,
     help="Background color of image as RGB hex string.")
 
-parser.add_argument("--fill-color", nargs="?", default="000000",
+parser.add_argument("--fill-color", default="000000",
     dest="fill_color", type=color_hex_string,
     help="Color of character as RGB hex string.")
 
-parser.add_argument("--font-size", nargs="?", default=None,
+parser.add_argument("--font-size", default=None,
     dest="font_size", type=int,
     help="Size of character to draw in pixels. Defaults to half of image size.")
 
-parser.add_argument("--output", nargs=1,
+parser.add_argument("--output",
     dest="output", type=expand_path,
-    help="Path to output generated image.")
+    help="""Path to output generated image. If output is omitted, a preview will be shown. Note that
+    the preview mechanism may not support an alpha channel.""")
 
-parser.add_argument("--size", nargs="?", default=128,
+parser.add_argument("--size", default=128,
     dest="size", type=int,
     help="Dimensions of output image in pixels. Defaults to %(default)d pixels.")
 
 args = parser.parse_args()
-
-print(args)
 
 # Default font size to half of image size.
 if not args.font_size:
@@ -61,6 +67,6 @@ y = round((args.size - glyph_size[1]) / 2.0)
 draw.text((x, y), glyph, fill=args.fill_color, font=font)
 
 if args.output:
-    img.save(os.path.realpath(args.output[0]))
+    img.save(os.path.realpath(args.output))
 else:
     img.show()
