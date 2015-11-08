86f4d4d899391ee771fa26feec21d80588dc0feb
Nick Watts <nick@nawatts.com>
2015-11-07 21:53:16 -0500

    Add options to set background/foreground colors.

--- font2image.py
+++ font2image.py
@@ -6,6 +6,9 @@ import sys
 
 from PIL import Image, ImageDraw, ImageFont
 
+def color_hex_string(hex_string):
+    return (int(hex_string[0:2], 16), int(hex_string[2:4], 16), int(hex_string[4:6], 16))
+
 def expand_path(path):
     return os.path.realpath(os.path.expandvars(os.path.expanduser(path)))
 
@@ -16,6 +19,14 @@ parser.add_argument("font_file", type=expand_path,
 
 parser.add_argument("glyph", type=str,
     help="The Unicode code of the character to render. Ex: f0f3.")
+
+parser.add_argument("--background-color", nargs="?", default="ffffff",
+    dest="background_color", type=color_hex_string,
+    help="Background color of image as RGB hex string.")
+
+parser.add_argument("--fill-color", nargs="?", default="000000",
+    dest="fill_color", type=color_hex_string,
+    help="Color of character as RGB hex string.")
 
 parser.add_argument("--font-size", nargs="?", default=None,
     dest="font_size", type=int,
@@ -29,9 +40,9 @@ parser.add_argument("--size", nargs="?", default=128,
     dest="size", type=int,
     help="Dimensions of output image in pixels. Defaults to %(default)d pixels.")
 
-# TODO: Background/foreground color arguments. Support RGBA.
-
 args = parser.parse_args()
+
+print(args)
 
 # Default font size to half of image size.
 if not args.font_size:
@@ -40,14 +51,14 @@ if not args.font_size:
 glyph = bytes("\\u" + args.glyph, "ascii").decode("unicode-escape")
 
 font = ImageFont.truetype(os.path.realpath(args.font_file), size=args.font_size)
-img = Image.new("RGBA", (args.size, args.size), (255,255,255))
+img = Image.new("RGBA", (args.size, args.size), args.background_color)
 draw = ImageDraw.Draw(img)
 
 glyph_size = draw.textsize(glyph, font=font)
 x = round((args.size - glyph_size[0]) / 2.0)
 y = round((args.size - glyph_size[1]) / 2.0)
 
-draw.text((x, y), glyph, fill=(0, 0, 0), font=font)
+draw.text((x, y), glyph, fill=args.fill_color, font=font)
 
 if args.output:
     img.save(os.path.realpath(args.output[0]))
